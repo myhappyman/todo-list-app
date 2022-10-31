3cfd267810e989e23ba8d15c3a9bafe0e9d7486a
ParkShinwoo <myhappyman@naver.com>
2022-10-31 23:22:12 +0900

    setToDos에서 oldToDos를 통해 기존의 toDo리스트들 중에 방금 클릭한 요소의 위치를 찾을 수 있도록 findIndex메소드를 통해 내부에 함수를 정의하여 위치를 찾아내었다.

--- src/components/toDo.tsx
+++ src/components/toDo.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
-
-
+/**
+ * array안에 있는 값을 수정하기
+ * 1. 우리는 id로 toDo값을 찾아야한다.
+ * 2. 위치를 찾았으면 immutability의 규칙을 지키면서 배열의 값을 새롭게 생성해야한다.
+ * 3. const food = ["banana", "apple", "mango", "감", "김밥"];
+ * 4. apple의 값을 pear로 바꾼다고 했을때 apple앞과 뒤를 기준으로 쪼갠다.
+ * 5. const front = ["banana"];
+ * 6. const back = ["mango", "감", "김밥"];
+ * 7. const newArr = [...front, "pear", ...back];
+ * 
+ * @param param0 
+ * @returns 
+ */
 function ToDo({text, category, id}:IToDo){
     //const onClick = (newCategory:  "TODO" | "DOING" | "DONE") => {
     // const onClick = (newCategory:  IToDo["category"]) => {
@@ -13,6 +24,15 @@ function ToDo({text, category, id}:IToDo){
     const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
         const {currentTarget:{name}} = event;
         console.log("i wanna to ", name);
+        setToDos(oldToDos => {
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+            const oldTodo = oldToDos[targetIndex];
+            const newToDO = {text, id, category:name};
+            console.log(oldTodo);
+            console.log(newToDO);
+            return oldToDos;
+        });
+
     }
 
     //기존에 작성하던 방식인 onClick={onClick("TODO")} 이런 형태로는 인자가 넘겨지지 않는다.
